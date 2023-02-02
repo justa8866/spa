bcb7b31e178ed800a42bc3d7b8b41545dcb3a84d
Justyna Duranowska <justa8866@gmail.com>
2023-02-02 20:35:06 +0100

    feat: add mui, styled components, icons, font

---
+++ src/components/App/index.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { Box, Button, CssBaseline } from "@mui/material";
+
+function App() {
+  return (
+    <Box sx={{ display: "flex" }}>
+      <CssBaseline />
+      <Button>test</Button>
+    </Box>
+  );
+}
+
+export default App;
